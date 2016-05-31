f7c2d8fb445998f58194b750320fa3eae02532f2
cobwebkanamachi <cobwebkanamachi@users.noreply.github.com>
2016-05-31 13:25:33 +0900

    Update d.c

--- d.c
+++ d.c
@@ -3,7 +3,7 @@
   void f(void)
   {
      int* x = malloc(10 * sizeof(int));
-     x[8] = 0;        // problem 1: heap block overrun
+     x[8] = 0;        // problem 1: heap block overrun(valgrind watch, but safe in this case. write x[10] cause error)
   }
 
   int main(void)
